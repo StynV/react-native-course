63efa9de95c253d2dcda561e1170e0dfbbd6e1f1
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-11 16:57:46 +0100

    Style input field

--- starting-project/screens/StartGameScreen.js
+++ starting-project/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -22,6 +28,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    textAlign: 'center',
   },
 });
 

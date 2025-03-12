f6ffcae6e85aae8d0269e0d2b2daf78e99f326c3
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-12 14:11:25 +0100

    Make sure we're under the notch

--- starting-project/App.js
+++ starting-project/App.js
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -24,7 +24,7 @@ export default function App() {
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- starting-project/screens/GameScreen.js
+++ starting-project/screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      <View>
+        <Text>Log rounds</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;

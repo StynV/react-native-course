579f74437ab9924ee0b0e62284dbda97919e0e5a
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-12 12:53:00 +0100

    Start using multiple screens

--- starting-project/screens/StartGameScreen.js
+++ starting-project/screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 import { useState } from 'react';
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   const numberInputHandler = inputText => {
@@ -22,6 +22,8 @@ function StartGameScreen() {
       ]);
       return;
     }
+
+    onPickNumber(enteredNumber);
   };
 
   return (

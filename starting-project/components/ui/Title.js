c6a9cbd49bd1e9c9f6c418a8d81206e951761d5c
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-13 14:52:42 +0100

    Add custom fonts

--- starting-project/components/ui/Title.js
+++ starting-project/components/ui/Title.js
@@ -4,8 +4,8 @@ const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 18,
-    fontWeight: 'bold',
     color: 'white',
     textAlign: 'center',
     borderWidth: 2,

4d2aedc8dd8912e22cc0886cf1e435addad28206
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-14 14:03:40 +0100

    add dark bg

--- starting-project-navigation/App.js
+++ starting-project-navigation/App.js
@@ -1,7 +1,13 @@
 import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { StatusBar } from 'expo-status-bar';
 
-const App = () => <CategoriesScreen />;
+const App = () => (
+  <>
+    <StatusBar style="light" />
+    <CategoriesScreen />
+  </>
+);
 
 const styles = StyleSheet.create({
   container: {},

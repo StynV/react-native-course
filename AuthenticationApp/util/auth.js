b2c74d16f005385a7c63027916a22c193c66c82f
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-19 11:47:13 +0100

    Implement Login

--- AuthenticationApp/util/auth.js
+++ AuthenticationApp/util/auth.js
@@ -2,13 +2,22 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyBU1UspouZtVsQt8fAs9fmP5TFKWv-3_Vc';
 
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const result = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(result.data);
+};
+
 export const createUser = async (email, password) => {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    },
-  );
+  authenticate('signUp', email, password);
+};
+
+export const login = async (email, password) => {
+  authenticate('signInWithPassword', email, password);
 };

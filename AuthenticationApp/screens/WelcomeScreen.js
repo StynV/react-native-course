19d1a33d2d5dec6e663b29b82b36820485943302
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-03-19 12:18:00 +0100

    Fetch protected data from firebase

--- AuthenticationApp/screens/WelcomeScreen.js
+++ AuthenticationApp/screens/WelcomeScreen.js
@@ -1,11 +1,33 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
-const WelcomeScreen = () => (
-  <View style={styles.rootContainer}>
-    <Text style={styles.title}>Welcome!</Text>
-    <Text>You authenticated successfully!</Text>
-  </View>
-);
+const WelcomeScreen = () => {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://react-native-1ec13-default-rtdb.europe-west1.firebasedatabase.app/Message.json?auth=' +
+          token,
+      )
+      .then(response => {
+        setFetchedMessage(response.data);
+      });
+  }, []);
+
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.title}>Welcome!</Text>
+      <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   rootContainer: {
